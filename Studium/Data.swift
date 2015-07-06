b44f847699e6011226ff1465f0863b616b5b480e
Troy Stribling <troy.stribling@gmail.com>
2015-07-06 08:34:41 -0700

    more work on transaction class

--- Studium/Data.swift
+++ Studium/Data.swift
@@ -10,9 +10,9 @@ import Foundation
 
 public struct Data {
     
-    public class Transaction {
+    public class Transaction : Comparable {
         
-        let name:String, amount:Float, date:NSDate
+        let who:String, when:NSDate, amount:Float
         
         public class func load(file:String) -> [Transaction] {
             let dateFormatter = NSDateFormatter()
@@ -22,15 +22,15 @@ public struct Data {
                 let regex = try NSRegularExpression(pattern:"\\s+", options:[])
                 let rows = split(data.characters){$0=="\n"}.map {slice -> [String] in
                     let row = NSMutableString()
-                        row.setString(String(slice))
-                        regex.replaceMatchesInString(row, options:[], range:NSMakeRange(0, row.length-1), withTemplate:",")
-                        return split(String(row).characters){$0==","}.map(String.init)
-                    }
+                    row.setString(String(slice))
+                    regex.replaceMatchesInString(row, options:[], range:NSMakeRange(0, row.length-1), withTemplate:",")
+                    return split(String(row).characters){$0==","}.map(String.init)
+                }
                 var trans = [Transaction]()
                 for row in rows {
                     if row.count == 3 {
                         if let date = dateFormatter.dateFromString(row[1]), amount = Float(row[2]) {
-                            trans.append(Transaction(name:row[0], date:date, amount:amount))
+                            trans.append(Transaction(who:row[0], when:date, amount:amount))
                         }
                     }
                 }
@@ -40,12 +40,54 @@ public struct Data {
             }
         }
         
-        public init(name:String, date:NSDate, amount:Float) {
-            self.name = name
+        public init(who:String, when:NSDate, amount:Float) {
+            self.who = who
             self.amount = amount
-            self.date = date
+            self.when = when
         }
         
+//        public convenience init?(row:String) {
+//            let dateFormatter = NSDateFormatter()
+//            dateFormatter.dateFormat = "MM/dd/yyyy"
+//            let mutableRow = NSMutableString()
+//            mutableRow.setString(row)
+//            do {
+//                let regex = try NSRegularExpression(pattern:"\\s+", options:[])
+//                regex.replaceMatchesInString(mutableRow, options:[], range:NSMakeRange(0, mutableRow.length-1), withTemplate:",")
+//                let items = split(String(row).characters){$0==","}.map(String.init)
+//                if items.count == 3 {
+//                    if let when = dateFormatter.dateFromString(items[1]), amount = Float(items[2]) {
+//                        self.init(who:items[0], when:when, amount:amount)
+//                    }
+//                }
+//            } catch {
+//                return nil
+//            }
+//
+//        }
     }
     
 }
+
+// equitable
+public func ==(lhs:Data.Transaction, rhs:Data.Transaction) -> Bool {
+    return lhs.amount == rhs.amount
+}
+
+// comparable
+public func <(lhs:Data.Transaction, rhs:Data.Transaction) -> Bool {
+    return lhs.amount < rhs.amount
+}
+
+public func <=(lhs:Data.Transaction, rhs:Data.Transaction) -> Bool {
+    return lhs.amount <= rhs.amount
+}
+
+public func >=(lhs:Data.Transaction, rhs:Data.Transaction) -> Bool {
+    return lhs.amount >= rhs.amount
+}
+
+public func >(lhs:Data.Transaction, rhs:Data.Transaction) -> Bool {
+    return lhs.amount > rhs.amount
+}
+
