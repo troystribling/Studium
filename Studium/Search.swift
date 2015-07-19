c62730dd18697eb9f01bd9122f4ca2fbe6f18232
troy Stribling <troy.stribling@gmail.com>
2015-07-18 18:01:10 -0700

    added verbage

--- Studium/Search.swift
+++ Studium/Search.swift
@@ -8,26 +8,85 @@
 
 import Foundation
 
+
+// Union Find constructs a connected network of nodes
 public protocol UnionFind {
     
+    // number of connected networks
     var count : Int     {get}
-    var nodes : [Int]   {get}
     
+    // network node ids are array index but intepretation of value depnds on implementation but it always identifies the 
+    // network containing the node. All nodes with same network identifier are in the same network
+    var network : [Int]   {get}
+    
+    // true if nodes with ids p and q are in the same network
     func connected(p:Int, q:Int) -> Bool
+    
+    // return the network identifier for node with id q
     func find(p:Int) -> Int
+    
+    // add network connection between nodes with the ids p in q
     func union(p:Int, q:Int)
     
 }
 
 public extension UnionFind {
     
+    // two nodes in the network are network are considered connected if their network idetifiers are equal
     func connected(p:Int, q:Int) -> Bool {
         return self.find(p) == self.find(q)
     }
     
 }
 
-public class UnionQuickFind : UnionFind {
+// Quick Find implementation optimizes find but slow union
+public class UnionFindQuickFind : UnionFind {
+
+    // array indexed by node value. For quick find implemenation network identifier is stored at node id
+    private var ids : [Int] = []
+    
+    // number of networks
+    private var n : Int = 0
+    
+    public var count : Int {
+        return self.n
+    }
+    
+    public var network : [Int] {
+        return self.ids
+    }
+    
+    public init() {
+    }
+    
+    public init(n:Int) {
+        self.n = n
+        self.ids = ArrayTools.fillUpTo(n)
+    }
+    
+    // network identifier is stored at node identier
+    public func find(p:Int) -> Int {
+        return self.ids[p]
+    }
+    
+    // cooenct 2 nodes in network
+    public func union(p:Int, q:Int) {
+        let pid = self.find(p), qid = find(q)
+        // return if nodes are in same netwok
+        guard pid != qid else {
+            return
+        }
+        // loop through all nodes and add p network to q network
+        for i in (0..<ids.count) {
+            if self.ids[i] == pid {
+                self.ids[i] = qid
+            }
+        }
+        --self.n
+    }
+}
+
+public class UnionFindQuickUnion : UnionFind {
     
     private var ids : [Int] = []
     private var n : Int = 0
@@ -40,28 +99,14 @@ public class UnionQuickFind : UnionFind {
         return self.ids
     }
     
-    public init() {
-    }
-    
     public init(n:Int) {
         self.n = n
         self.ids = ArrayTools.fillUpTo(n)
     }
     
     public func find(p:Int) -> Int {
-        return self.ids[p]
     }
     
     public func union(p:Int, q:Int) {
-        let pid = self.find(p), qid = find(q)
-        guard pid != qid else {
-            return
-        }
-        for i in (0..<ids.count) {
-            if self.ids[i] == pid {
-                self.ids[i] = qid
-            }
-        }
-        --self.n
     }
 }
