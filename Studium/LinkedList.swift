0d229ae3b21579bee023c738032da55b17325d56
Troy Stribling <troy.stribling@gmail.com>
2015-07-19 17:40:49 -0700

    added linked list verbage

--- Studium/LinkedList.swift
+++ Studium/LinkedList.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
+// node in linked list containing node item and reference to next node in list
 public class LinkedListNode<T> {
     public let item : T
     internal var next : LinkedListNode?
@@ -17,10 +18,16 @@ public class LinkedListNode<T> {
     }
 }
 
+// linked list implementaion combining stack and queue interfaces to add and remove items form list
 public class LinkedList<T> : SequenceType {
     
+    // refernec to first node in list
     internal var first : LinkedListNode<T>?
+    
+    // refernce to last node in list
     internal var last : LinkedListNode<T>?
+    
+    // number of node in list
     private var n : Int = 0
     
     public var size : Int {
@@ -34,6 +41,9 @@ public class LinkedList<T> : SequenceType {
     public init() {
     }
     
+    // Add item to begging of list. Create a new node using given item and set its next reference
+    // to currect first node then reassign first reference to new node. If the list is empty also set the 
+    // reference to the last node in the list to the new node.
     public func push(item:T) {
         let newNode = LinkedListNode(item:item)
         newNode.next = self.first
@@ -44,6 +54,8 @@ public class LinkedList<T> : SequenceType {
         ++self.n
     }
     
+    // Remove first item from list. Reassign the first node to the removed node next reference. If removing the node
+    // empties the list set the reference to the last node to nil.
     public func pop() -> T? {
         let oldNode = self.first
         self.first = oldNode?.next
@@ -54,6 +66,9 @@ public class LinkedList<T> : SequenceType {
         return oldNode?.item
     }
     
+    // Add an item to the end of the list. Save the current first node then reassign the first node to a newly created
+    // node using the gevin item. If the list is empty set the refrence to the first node to the last node. If the list
+    // is not empty assign the next node reference of the previous last not to the new last node.
     public func enqueue(item:T) {
         let oldlast = self.last
         self.last = LinkedListNode(item:item)
@@ -65,6 +80,7 @@ public class LinkedList<T> : SequenceType {
         ++self.n
     }
     
+    // Remove item from beginning of list. Implementaion is the same as pop
     public func dequeue() -> T? {
         return self.pop()
     }
@@ -75,6 +91,7 @@ public class LinkedList<T> : SequenceType {
     }
 }
 
+// Linked list generator
 public struct LinkeListGenerator<T> : GeneratorType {
 
     let list : LinkedList<T>
