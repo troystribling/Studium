ebe20f597d5e6a9eb8ea79eed6632108f7ac4f7a
troy Stribling <troy.stribling@gmail.com>
2015-07-20 08:37:49 -0700

    added example for all sorts to playground

--- Sort.playground/Contents.swift
+++ Sort.playground/Contents.swift
@@ -3,10 +3,68 @@
 import UIKit
 import Studium
 
+// Selection Sort
+print("Selection Sort")
 var vals = ArrayTools.fillUpTo(30)
 ArrayTools.shuffle(&vals)
-print(vals)
-print(ArrayTools.sorted(vals))
+print("Presort:\(ArrayTools.sorted(vals))")
+Sort.Selection.sort(&vals)
+print("Postsort:\(ArrayTools.sorted(vals))")
+
+// Insertion Sort
+print("\nInsertion Sort")
+vals = ArrayTools.fillUpTo(30)
+ArrayTools.shuffle(&vals)
+print("Presort:\(ArrayTools.sorted(vals))")
+Sort.Insertion.sort(&vals)
+print("Postsort:\(ArrayTools.sorted(vals))")
+
+// Shell Sort
+print("\nShell Sort")
+vals = ArrayTools.fillUpTo(30)
+ArrayTools.shuffle(&vals)
+print("Presort:\(ArrayTools.sorted(vals))")
+Sort.Shell.sort(&vals)
+print("Postsort:\(ArrayTools.sorted(vals))")
+
+// Topdown Merge Sort
+print("\nTopdown Merge Sort")
+vals = ArrayTools.fillUpTo(30)
+ArrayTools.shuffle(&vals)
+print("Presort:\(ArrayTools.sorted(vals))")
+Sort.Merge.TopDown.sort(&vals)
+print("Postsort:\(ArrayTools.sorted(vals))")
+
+
+// Bottom up Merge Sort
+print("\nBottom Up Sort")
+vals = ArrayTools.fillUpTo(30)
+ArrayTools.shuffle(&vals)
+print("Presort:\(ArrayTools.sorted(vals))")
+Sort.Merge.BottomUp.sort(&vals)
+print("Postsort:\(ArrayTools.sorted(vals))")
+
+
+// Quick Sort
+print("\nQuick Sort")
+vals = ArrayTools.fillUpTo(30)
+ArrayTools.shuffle(&vals)
+print("Presort:\(ArrayTools.sorted(vals))")
+Sort.Quick.sort(&vals)
+print("Postsort:\(ArrayTools.sorted(vals))")
+
+// 3Way Quick Sort
+print("\n3Way Quick Sort")
+var vals32 = ArrayTools.fillRandom(30, upToValue:4)
+ArrayTools.shuffle(&vals32)
+print("Presort:\(ArrayTools.sorted(vals32))")
+Sort.Quick3way.sort(&vals32)
+print("Postsort:\(ArrayTools.sorted(vals32))")
+
+// Heap Sort
+print("\nHeap Sort")
+vals = ArrayTools.fillUpTo(30)
+ArrayTools.shuffle(&vals)
+print("Presort:\(ArrayTools.sorted(vals))")
 Sort.Heap.sort(&vals)
-print(vals)
-print(ArrayTools.sorted(vals))
+print("Postsort:\(ArrayTools.sorted(vals))")
